c7418aeb432cdafdd316004b1fe36bc558192f97
Mati <mrgnzbalbi@gmail.com>
2025-11-15 12:23:08 -0300

    Generales

--- v2_NN/test_entrenamiento.py
+++ v2_NN/test_entrenamiento.py
@@ -5,10 +5,8 @@ from chess_game import ChessGame
 from model import create_chess_model
 from alpha_zero import AlphaZero
 
-# Configuración de promociones
 INCLUDE_QUEEN_PROMOTIONS = False
 
-# Setup
 game = ChessGame(include_queen_promotions=INCLUDE_QUEEN_PROMOTIONS)
 model = create_chess_model(game=game, num_resBlocks=2, num_hidden=32)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)

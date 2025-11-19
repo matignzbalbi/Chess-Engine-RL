30eb414b62ed91351fa4b0cd7d6cc841b7f5bc10
Mati <mrgnzbalbi@gmail.com>
2025-11-19 12:06:19 -0300

    Creación de .JOBS

---
+++ v2_NN/entrenamiento_3.py
@@ -0,0 +1,31 @@
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+import torch
+from chess_game import ChessGame
+from model import create_chess_model
+from alpha_zero import AlphaZero
+
+INCLUDE_QUEEN_PROMOTIONS = False
+
+game = ChessGame(include_queen_promotions=INCLUDE_QUEEN_PROMOTIONS)
+model = create_chess_model(game=game, num_resBlocks=10, num_hidden=256)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
+
+args = {
+    'C': 2,
+    'num_searches': 180,
+    'num_selfPlay_iterations': 45,
+    'num_iterations': 35,
+    'num_epochs': 7,
+    'batch_size': 192,
+    'save_every': 3
+}
+
+logging.info(f"Action size: {game.action_size}")
+logging.info(f"Model params: {sum(p.numel() for p in model.parameters()):,}")
+logging.info(f"Config: {args['num_iterations']} iterations, {args['num_searches']} searches/move\n")
+
+alphaZero = AlphaZero(model, optimizer, game, args)
+alphaZero.learn()
+
+logging.info("Finalizado")
